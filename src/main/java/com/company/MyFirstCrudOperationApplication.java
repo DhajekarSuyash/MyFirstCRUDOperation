5d68afd7976220bd44d8f6192a0a3ae2879926d5
DhajekarSuyash <suyash.dhajekar18@gmail.com>
2025-08-21 20:49:55 +0530

    schedulars added

--- src/main/java/com/company/MyFirstCrudOperationApplication.java
+++ src/main/java/com/company/MyFirstCrudOperationApplication.java
@@ -2,10 +2,12 @@ package com.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableScheduling
 public class MyFirstCrudOperationApplication {
 
 	public static void main(String[] args) {
